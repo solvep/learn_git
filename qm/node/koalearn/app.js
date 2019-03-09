bb69335c934b8e5cb018044c6a80f63ea321ecb0
solvep <124667390@qq.com>
2019-03-09 23:22:21 +0800

    'koa'

---
+++ qm/node/koalearn/app.js
@@ -0,0 +1,14 @@
+const Koa = require('koa');
+const app = new Koa();
+
+
+app.use(async (ctx,next)=>{
+ console.log(ctx.response);
+ if(ctx.url === '/'){
+   ctx.response.redirect('/login')
+ }else{
+   ctx.body = '我是登陆界面'
+ }
+})
+
+app.listen(3000)
